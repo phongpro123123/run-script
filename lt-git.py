cc85b1ecfbfab67a4944f4068b6b8023cdd02f2c
phongpro123123 <40991303+phongpro123123@users.noreply.github.com>
2024-07-17 15:20:43 +0700

    Update lt-git.py

--- lt-git.py
+++ lt-git.py
@@ -261,4 +261,4 @@ def download_file(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')  # Add host='0.0.0.0' to allow external access
